72a5ea3590497392653594a2ff2597aabb5c4e91
jiucheng <15279232381@163.com>
2020-07-17 11:32:54 +0800

    资讯列表及详情页面功能完成

--- pages/news/news.vue
+++ pages/news/news.vue
@@ -1,22 +1,55 @@
 <template>
-	<view>
-		资讯页面
+	<view class="news">
+		<!-- <view class="news-item" v-for="(item,index) in newsList" :key="index">
+			<image :src="item.img_url"></image>
+			<view class="right">
+				<view class="title">
+					{{ item.title }}
+				</view>
+				<view class="info">
+					<text>发表时间：{{ item.add_time }}</text>
+					<text>浏览：{{ item.click }}</text>
+				</view>
+			</view>
+		</view> -->
+		<newsItem :newsList="newsList" @itemClick="goDetail"></newsItem>
 	</view>
 </template>
 
 <script>
+	import newsItem from '../../components/news-item/news-item.vue'
+	
 	export default {
 		data() {
 			return {
-				
+				newsList: []
 			}
 		},
+		components: {
+			newsItem
+		},
+		onLoad() {
+			this.getNews()
+		},
 		methods: {
-			
+			async getNews() {
+				const res = await this.$myRequest({
+					url: '/api/getnewslist'
+				})
+				// console.log(res)
+				this.newsList = res.data.message
+			},
+			goDetail(id) {
+				uni.navigateTo({
+					url: '/pages/news-detail/news-detail?id=' + id
+				})
+			}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="less">
+	.news {
+		
+	}
 </style>
